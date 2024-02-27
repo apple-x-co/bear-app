e9110d0147b075461bc81cc76ff2892d083c4d9d
Kouhei.Sano <apple.x.co@gmail.com>
2024-02-27 18:12:17 +0900

    chore:

--- source/app/public/index.php
+++ source/app/public/index.php
@@ -8,4 +8,4 @@ session_name('bear-app');
 
 require dirname(__DIR__) . '/autoload.php';
 //exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'hal-app' : 'prod-hal-app', $GLOBALS, $_SERVER)); // old
-exit((new Bootstrap())(((PHP_SAPI === 'cli-server' || getenv('APP_ENV') === '1') ? '' : 'prod-') . (str_contains($_SERVER['HTTP_ACCEPT'], 'application/json') ? 'hal-api-app' : 'html-app'), $GLOBALS, $_SERVER));
+exit((new Bootstrap())(((PHP_SAPI === 'cli-server' || getenv('APP_ENV') === 'local') ? '' : 'prod-') . (str_contains($_SERVER['HTTP_ACCEPT'], 'application/json') ? 'hal-api-app' : 'html-app'), $GLOBALS, $_SERVER));
