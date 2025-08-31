0bbcaca47cb33b6b00b34616c3bbb5d3515dfb92
Kouhei.Sano <apple.x.co@gmail.com>
2025-08-31 10:31:41 +0900

    refactor: replace vendor-specific exception with application-specific `RuntimeException` in `CaptchaException`

--- source/app/ddd/core/src/Domain/Captcha/CaptchaException.php
+++ source/app/ddd/core/src/Domain/Captcha/CaptchaException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace AppCore\Domain\Captcha;
 
-use MyVendor\MyProject\Exception\RuntimeException;
+use AppCore\Exception\RuntimeException;
 
 class CaptchaException extends RuntimeException
 {
