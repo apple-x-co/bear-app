ccf15af6209f30354ab7b52de57debf20e9529fc
Kouhei.Sano <apple.x.co@gmail.com>
2025-09-25 10:32:43 +0900

    refactor: streamline environment handling and update `APP_CONTEXT` usage in bootstrap logic

--- source/app/bin/command.php
+++ source/app/bin/command.php
@@ -5,4 +5,11 @@ declare(strict_types=1);
 use MyVendor\MyProject\Bootstrap;
 
 require dirname(__DIR__) . '/autoload.php';
-exit((new Bootstrap())('cli-command-app', $GLOBALS, $_SERVER));
+
+$environment = getenv('APP_CONTEXT') ?: (PHP_SAPI === 'cli-server' ? 'dev' : 'prod');
+$apiType = 'cli-command-app';
+$context = ($environment === 'prod' ? 'prod-' : '') . $apiType;
+
+exit((new Bootstrap())($context, $GLOBALS, $_SERVER));
+
+// exit((new Bootstrap())('cli-command-app', $GLOBALS, $_SERVER));
--- source/app/public/index.php
+++ source/app/public/index.php
@@ -7,5 +7,11 @@ use MyVendor\MyProject\Bootstrap;
 session_name('bear-app');
 
 require dirname(__DIR__) . '/autoload.php';
+
+$environment = getenv('APP_CONTEXT') ?: (PHP_SAPI === 'cli-server' ? 'dev' : 'prod');
+$apiType = str_contains($_SERVER['HTTP_ACCEPT'] ?? '', 'application/json') ? 'hal-api-app' : 'html-app';
+$context = ($environment === 'prod' ? 'prod-' : '') . $apiType;
+
+exit((new Bootstrap())($context, $GLOBALS, $_SERVER));
+
 //exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'hal-app' : 'prod-hal-app', $GLOBALS, $_SERVER)); // old
-exit((new Bootstrap())(((PHP_SAPI === 'cli-server' || getenv('APP_ENV') === 'local') ? '' : 'prod-') . (str_contains($_SERVER['HTTP_ACCEPT'], 'application/json') ? 'hal-api-app' : 'html-app'), $GLOBALS, $_SERVER));
