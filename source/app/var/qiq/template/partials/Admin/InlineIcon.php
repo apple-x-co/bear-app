a12028b986df3e55895762cbf1c7a4ca898412a1
Kouhei.Sano <apple.x.co@gmail.com>
2025-12-02 11:00:56 +0900

    refactor: add support for customizable icon stroke widths via CSS classes and utility variables

--- source/app/var/qiq/template/partials/Admin/InlineIcon.php
+++ source/app/var/qiq/template/partials/Admin/InlineIcon.php
@@ -1,5 +1,6 @@
 {{ $w = isset($w) ? $w : 6 }}
 {{ $h = isset($h) ? $h : 6 }}
-<svg class="inline w-{{a $w }} h-{{a $h }}">
+{{ $stroke = isset($stroke) ? $stroke : 'stroke-[1.5px]' }}
+<svg class="inline w-{{a $w }} h-{{a $h }} {{a $stroke }}">
     <use xlink:href="/admin/images/heroicons/icon.svg#{{a $name }}"></use>
 </svg>
